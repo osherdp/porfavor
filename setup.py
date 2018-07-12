762d1cb221e8440ad0a497c6d4482d1e7e0f41fb
Osher De Paz <osherdepaz@gmail.com>
2018-07-12 19:55:34 +0300

    bump version and fix classifiers

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as readme:
 
 setuptools.setup(
     name="porfavor",
-    version="0.1.0",
+    version="0.3.0",
     author="osherdp",
     author_email="osherdepaz@gmail.com",
     url="https://github.com/osherdp/porfavor",
@@ -23,10 +23,15 @@ setuptools.setup(
             "porfavor= porfavor:main",
         ]
     },
+
+    python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*",
     install_requires=["flask", "tqdm", "rpyc", "docopt"],
 
     classifiers=(
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ),
