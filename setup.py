50ee4e6cfc1c8cecdb48472f6a62f28dad30c3ad
Osher De Paz <osherdepaz@gmail.com>
2018-07-13 15:53:36 +0300

    Support Python 3.5 as well

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setuptools.setup(
         ]
     },
 
-    python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*",
+    python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*",
     install_requires=["flask",
                       "click",
                       "requests",
@@ -35,6 +35,7 @@ setuptools.setup(
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
